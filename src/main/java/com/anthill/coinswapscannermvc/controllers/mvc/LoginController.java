f296c00510cf299c9f280b43e7dbd0aecc847bda
rocket-builder <53790403+rocket-builder@users.noreply.github.com>
2021-09-09 22:19:24 +0300

    add index mappings

--- src/main/java/com/anthill/coinswapscannermvc/controllers/mvc/LoginController.java
+++ src/main/java/com/anthill/coinswapscannermvc/controllers/mvc/LoginController.java
@@ -10,9 +10,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpSession;
 
@@ -22,7 +20,7 @@ public class LoginController {
     @Autowired
     UserRepos userRepos;
 
-    @GetMapping("/login")
+    @RequestMapping(value = {"/", "/index", "login"}, method = RequestMethod.GET)
     public String index(){
         return "index";
     }
